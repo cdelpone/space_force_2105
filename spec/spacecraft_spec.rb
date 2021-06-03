a1cf4d0e83e69fe566759ed39275df396d68af8e
Christina Delpone <cdelpone@gmail.com>
2021-06-03 09:12:00 -0600

    Inital commit

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,9 @@
+require './spec_helper'
+
+RSpec.describe Spacecraft do
+  it 'exists' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus).to be_an_instance_of(Spacecraft)
+  end
+end
